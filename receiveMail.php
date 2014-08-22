9ca3b60e419e9dfb0d0746c1998b6ef72cbd5f96
Tinashe Matate <tm57@users.noreply.github.com>
2014-08-22 05:40:41 +0200

    Update receiveMail.php

--- receiveMail.php
+++ receiveMail.php
@@ -23,8 +23,7 @@ if(IsInjected($visitor_email))
 
 $email_from = 'tmatate.act@gmail.com';//<== update the email address
 $email_subject = "New Form submission";
-$email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n $message".
+$email_body = "You have received a new message from the user $name.\n Here is the message:\n $message";
     
 $to = "tmatate.act@gmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
