fa9fa01b5a8bf60b1c75f49e1da2565bdb07b4e3
tm57 <tmatate.act@gmail.com>
2014-08-22 04:14:20 +0200

    test2

---
+++ receiveMail.php
@@ -0,0 +1,11 @@
+$name = $_POST["name"];
+$visitor_email = $POST["email"];
+$msg = $_POST["comment"];
+$from = "tmatate.act@gmail.com";
+$subject = "NEW FORM SUBMISSION";
+$body = "$name has sent you a message.\n".
+    "email address: $visitor_email\n".
+    "Here is the message:\n $msg".
+$to = "tmatate.act@gmail.com";
+$headers = "From: $from\r\n".;
+mail($to,$subject,$body,$headers);
