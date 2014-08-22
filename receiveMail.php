75a234820d9edea3a65cfae718863dc24aacbcb0
tm57 <tmatate.act@gmail.com>
2014-08-22 04:57:39 +0200

    test3

--- receiveMail.php
+++ receiveMail.php
@@ -1,11 +1,13 @@
-$name = $_POST["name"];
-$visitor_email = $POST["email"];
-$msg = $_POST["comment"];
-$from = "tmatate.act@gmail.com";
-$subject = "NEW FORM SUBMISSION";
-$body = "$name has sent you a message.\n".
-    "email address: $visitor_email\n".
-    "Here is the message:\n $msg".
-$to = "tmatate.act@gmail.com";
-$headers = "From: $from\r\n".;
-mail($to,$subject,$body,$headers);
+<?php
+	$name = $_POST["name"];
+	$visitor_email = $POST["email"];
+	$msg = $_POST["comment"];
+	$from = "tmatate.act@gmail.com";
+	$subject = "NEW FORM SUBMISSION";
+	$body = "$name has sent you a message.\n.
+	    email address: $visitor_email.\n
+	    Here is the message:\n $msg";
+	$to = "tmatate.act@gmail.com";
+	$headers = "From: $from\r\n".;
+	mail($to,$subject,$body,$headers);
+?>
